8f05c9a3b3b56e8e23130761c4b6a132699a7a6b
Alex <radist-electric@yandex.ru>
2018-12-15 02:37:06 +0500

    My first commit

--- src/App.js
+++ src/App.js
@@ -2,27 +2,35 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Grid from 'react-bootstrap/lib/Grid';
+import Row from 'react-bootstrap/lib/Row';
+import Col from 'react-bootstrap/lib/Col';
+import styled from 'styled-components'
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="wrapper">
+        <SayHello word="World" />
+        <SayFullName name="Alex" surname="Raimov" link="https://vk.com/spacehuman" />
+        <SayFullName name="Anna" surname="Raimova" link="https://vk.com" />
+        <SayFullName name="Vasya" surname="Petrov" link="https://ok.ru" />
       </div>
     );
   }
+}
+function SayFullName(props) {
+  return (
+      <div>
+        <h2>Моё имя {props.name}, фамилия - {props.surname}</h2>
+        <a href={props.link}>Ссылка на мой профиль </a>
+      </div>
+    )
+}
+function SayHello(say) {
+  return (
+      <h1>Hello, {say.word}!</h1>
+    )
 }
 
 export default App;
